48a38b1db5d985ab5c71323497aeae198f2ce77d
Michael James <micrjames921@gmail.com>
2022-10-27 10:53:10 -0700

    wrote code for a class with overloaded constructors; implemented Pizza class for one with toppings and one without toppings

--- OOP/Main.java
+++ OOP/Main.java
@@ -1,21 +1,9 @@
 public class Main {
    public static void main(String[] args) {
-	   Car myCar = new Car("Chevrolet", "Corvette", 2020, "blue", 50000.00);
-	   Car otherCar = new Car("Ford", "Taurus", 1999, "green", 17000.00);
+	   Pizza pepperoni = new Pizza("thick crust", "tomato", "mozzarella", "pepperoni");
+	   Pizza cheese = new Pizza("pan crust", "tomato", "mozzarella");
 
-	   System.out.println(myCar.make);
-	   System.out.println(myCar.model);
-
-	   myCar.drive();
-	   myCar.brake();
-
-	   System.out.println(otherCar.make);
-	   System.out.println(otherCar.model);
-
-	   otherCar.drive();
-	   otherCar.brake();
-
-	   DiceRoller dr = new DiceRoller();
-	   System.out.println(String.format("You rolled a %d.", dr.roll()));
+	   System.out.println(pepperoni.desc());
+	   System.out.println(cheese.desc());
    }
 }
