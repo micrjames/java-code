50c73e7fe8810b8f3092e5755e5fbca917053463
Michael James <micrjames921@gmail.com>
2022-10-27 14:00:25 -0700

    wrote code that displayed static fields and methods to implement a class that allows cars to be parked to a specified limit

--- OOP/Garage.java
+++ OOP/Garage.java
@@ -1,5 +1,23 @@
 public class Garage {
+    static int numberOfPkgStalls;
+	int totalNumStalls;
+
+	Garage(int totalNumStalls) {
+	    this.totalNumStalls = totalNumStalls;
+	}
+
     void park(Car car) {
-	    System.out.println(String.format("The %s %s is parked in the garage.", car.make, car.model));
+	    String pkgMsg;
+	    if(numberOfPkgStalls < this.totalNumStalls) {
+		   pkgMsg = String.format("The %s %s is parked in the garage.", car.make, car.model);
+		   numberOfPkgStalls++;
+		} else {
+		   pkgMsg = "The garage is full.";
+		}
+		System.out.println(pkgMsg);
+	}
+
+	static int displayNumStalls() {
+	    return numberOfPkgStalls;
 	}
 }
--- OOP/Main.java
+++ OOP/Main.java
@@ -1,30 +1,34 @@
 public class Main {
    public static void main(String[] args) {
-	   Pizza pepperoni = new Pizza("thick crust", "tomato", "mozzarella", "pepperoni");
-	   Pizza cheese = new Pizza("pan crust", "tomato", "mozzarella");
+	   Car taurus = new Car("Ford", "Taurus", 1992, "green", 12000.00);
+	   Car mustang = new Car("Ford", "Mustang", 2019, "yellow", 32000.00);
+	   Car camaro = new Car("Chevrolet", "Camaro", 2015, "red", 45000.00);
+	   Car beetle = new Car("Volkswagen", "Beetle", 1968, "green", 7000.00);
+	   Car bronco = new Car("Chevrolet", "Bronco", 1988, "white", 12000.00);
+	   Car sable = new Car("Mercury", "Sable", 1994, "dark green", 900.00);
 
-	   System.out.println(pepperoni);
-	   System.out.println(cheese);
+	   System.out.println(taurus);
+	   System.out.println(mustang);
+	   System.out.println(camaro);
+	   System.out.println(beetle);
+	   System.out.println(bronco);
+	   System.out.println(sable);
 
-	   Car car = new Car("Ford", "Taurus", 1992, "green", 12000.00);
-	   System.out.println(car);
+	   int numberOfPkgStalls = 5;
+	   Garage garage = new Garage(numberOfPkgStalls);
+	   garage.park(taurus);
+	   garage.park(mustang);
+	   garage.park(camaro);
+	   garage.park(beetle);
+	   garage.park(bronco);
+	   garage.park(sable);
 
-	   // Food[] refrigerator = new Food[3];
-	   Food pizza = new Food("pizza");
-	   Food burger = new Food("burger");
-	   Food sandwich = new Food("sandwich");
-
-	   // refrigerator[0] = pizza;
-	   // refrigerator[1] = burger;
-	   // refrigerator[2] = sandwich;
-
-	   Food[] refrigerator = {pizza, burger, sandwich};
-
-	   for(int i = 0; i < refrigerator.length; i++) {
-		   System.out.println(refrigerator[i]);
-	   }
-
-	   Garage garage = new Garage();
-	   garage.park(car);
+	   System.out.println(String.format("I have %d friends.", Friend.numberOfFriends));
+	   Friend friend1 = new Friend("Tony");
+	   System.out.println(String.format("I have %d friends.", Friend.numberOfFriends));
+	   Friend friend2 = new Friend("Paulie");
+	   System.out.println(String.format("I have %d friends.", Friend.numberOfFriends));
+	   Friend friend3 = new Friend("Bobby");
+	   System.out.println(String.format("I have %d friends.", Friend.numberOfFriends));
    }
 }
