3f3faaa0721902309c812fcbbfc2c9a1c9ff7b95
Michael James <micrjames921@gmail.com>
2022-10-27 08:21:03 -0700

    created some code exploring OOP in java

---
+++ OOP/Car.java
@@ -0,0 +1,15 @@
+public class Car {
+   	String make = "Chevrolet";
+	String model = "Corvette";
+	int year = 2020;
+	String color = "blue";
+	double price = 50000.00;
+
+	void drive() {
+	   	System.out.println("Drive!");
+	}
+
+	void brake() {
+	   	System.out.println("Brake!");
+	}
+}
