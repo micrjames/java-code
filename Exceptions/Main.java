64fc33c3deddbf1cfa184535c8dc45f1698cca9b
Michael James <micrjames921@gmail.com>
2022-11-20 14:28:57 -0700

    implemented some code: Exception handling

---
+++ Exceptions/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Main {
+   	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+	    try {
+		   System.out.println("Enter a whole number to divide: ");
+		   int x = scanner.nextInt();
+
+		   System.out.println("Enter a whole number to divide by: ");
+		   int y = scanner.nextInt();
+
+		   int z = x/y;
+		   System.out.println(String.format("result: %s", z));
+		} catch(ArithmeticException e) {
+		    System.out.println("You tried to divide by zero!");
+		} catch(InputMismatchException e) {
+		    System.out.println("Please enter a number.");
+		} catch(Exception e) {
+		    System.out.println("Something went wrong.");
+		} finally {
+		    scanner.close();
+		}
+	}
+}
