e565d6a4d956364a39f722495fc74e91428d9bb3
Michael James <micrjames921@gmail.com>
2024-02-29 19:58:03 -0700

    got working 'Box Layout' example

--- GUI/FrameDriver.java
+++ GUI/FrameDriver.java
@@ -1,14 +1,36 @@
 import java.awt.Color;
+import java.awt.Rectangle;
+import javax.swing.BoxLayout;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.BoxLayout;
+import javax.swing.border.EmptyBorder;
+import java.awt.Insets;
+import javax.swing.Box;
+import java.awt.Dimension;
 
 import Frame.Frame;
 import Label.Label;
+import Panel.Panel;
+import Button.Button;
 
 class FrameDriver {
    public static void main(String []args) {
-	  Frame frame = new Frame("A Frame.", false, 640, 480, new Color(123, 50, 250), null);
+	  Color purple = new Color(123, 50, 250);
+	  Frame frame = new Frame("A Frame.", false, 640, 480, purple, null);
 
-	  frame.<JLabel>add(new Label("Hello, World!", Color.black, 30, true));
+	  Panel panelMain = new Panel(new Rectangle(0, 0, 640, 480), purple);
+	  BoxLayout boxlayout = new BoxLayout(panelMain, BoxLayout.Y_AXIS);
+	  panelMain.setLayout(boxlayout);
+	  // panel.setBorder(new EmptyBorder(new Insets(100, 150, 100, 150)));
+	  panelMain.add(Box.createVerticalGlue());
+	  panelMain.<JLabel>add(new Label("Hello, World!", Color.black, 30, true));
+	  frame.<JPanel>add(panelMain);
+	  Panel panelBottom = new Panel(new Rectangle(0, 0, 50, 50), Color.WHITE);
+	  panelBottom.<JButton>add(new Button("click"));
+	  panelMain.<JPanel>add(panelBottom);
+
 	  frame.display();
    }
 }
--- GUI/Panel/Panel.java
+++ GUI/Panel/Panel.java
@@ -1,10 +1,16 @@
+package Panel;
+
 import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Rectangle;
 
-class Panel extends JPanel {
-   Panel(Rectangle rect, Color color) {
+public class Panel extends JPanel {
+   public Panel(Rectangle rect, Color color) {
 	  this.setBackground(color);
 	  this.setBounds(rect.x, rect.y, rect.width, rect.height);
+   }
+
+   public <T> void add(T component) {
+	  this.add(component);
    }
 }
