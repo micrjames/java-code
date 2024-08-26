4c529cefb38338362cbff6f1511e2b7d10fdd560
Michael James <micrjames921@gmail.com>
2024-08-25 23:47:43 -0700

    working through code; added defition to 'Expressions.java'

--- Expressions.java
+++ Expressions.java
@@ -1,5 +1,8 @@
 public class Expressions {
    public static void main(String[] args) {
+	  // expression = operands & operators
+	  // operands = values, variables, numbers, quantity
+	  // operators = + - * / %
 	  // int friends = 10;
 	  // friends = friends + 1; (// + - * / %)=
 	  // friends = friends / 3;
