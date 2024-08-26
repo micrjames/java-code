c2464cf5426bbfe8f028f3cff4111aab1e60ae2a
Michael James <micrjames921@gmail.com>
2024-08-26 02:01:55 -0700

    added code for Generics

---
+++ OOP/Generics/Vars.java
@@ -0,0 +1,13 @@
+public class Vars {
+   public static void main(String[] args) {
+	  MyIntegerClass myInt = new MyIntegerClass(1);
+	  MyDoubleClass myDouble = new MyDoubleClass(3.14);
+	  MyCharacterClass myChar = new MyCharacterClass('@');
+	  MyStringClass myString = new MyStringClass("Hello");
+
+	  System.out.println(myInt.getValue());
+	  System.out.println(myDouble.getValue());
+	  System.out.println(myChar.getValue());
+	  System.out.println(myString.getValue());
+   }
+}
